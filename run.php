f49333cb5b0d6815c630cbd43b4430c11dd724a2
Larry Garfield <larry@garfieldtech.com>
2016-12-12 15:06:54 -0600

    Run on the Platform.sh-specified port.

--- run.php
+++ run.php
@@ -12,7 +12,7 @@ $socket = new React\Socket\Server($loop);
 $http = new React\Http\Server($socket, $loop);
 
 $http->on('request', $app);
-echo "Server running at http://127.0.0.1:1337\n";
 
-$socket->listen(1337);
+$socket->listen(getenv('PORT'));
+
 $loop->run();
